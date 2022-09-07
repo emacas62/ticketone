531e5096e0a428ebfcc6dc10e41bf4315796b003
Emanuele Casciari <emanuele.casciari@ied.edu>
2022-09-07 11:27:35 +0200

    Scritto il comportamento delle funzioni del controller degli eventi

--- app/Http/Controllers/EventiController.php
+++ app/Http/Controllers/EventiController.php
@@ -3,61 +3,68 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Eventi;
 
 class EventiController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+        $eventi = Eventi::all();
+        return response()->json($eventi);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'title'=>'required',
+            'price'=>'required',
+            'description'=>'required'
+        ]);
+
+        $evento = new Eventi();
+
+        $evento->title = $request->input('title');
+        $evento->price = $request->input('price');
+        $evento->description = $request->input('description');
+
+        $evento->save();
+        return response()->json($evento);
+
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
-        //
+        $evento = Eventi::find($id);
+        return response()->json($evento);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'title'=>'required',
+            'price'=>'required',
+            'description'=>'required'
+        ]);
+
+        $evento = Eventi::find($id);
+
+        $evento->title = $request->input('title');
+        $evento->price = $request->input('price');
+        $evento->description = $request->input('description');
+
+        $evento->save();
+        return response()->json($evento);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function destroy($id)
     {
-        //
+        $evento = Eventi::find($id);
+        $evento->delete();
+        return response()->json("Evento eliminato con successo!");
     }
 }
