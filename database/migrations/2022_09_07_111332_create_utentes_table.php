78dad069c76b990694b77f1d578d97acf6210a01
Emanuele Casciari <emanuele.casciari@ied.edu>
2022-09-07 13:16:23 +0200

    Riaggiustato il modello di User

---
+++ database/migrations/2022_09_07_111332_create_utentes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utentes', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('email');
+            $table->string('password');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->date('birthDate');
+            $table->string('city');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utentes');
+    }
+};
