43dc023ace6f610da81a9de7bbd55305e09feb98
Emanuele Casciari <emanuele.casciari@ied.edu>
2022-09-21 17:02:04 +0200

    Sistemato qualcosa che mancava

--- app/Models/Utente.php
+++ app/Models/Utente.php
@@ -2,7 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
 
 class Utente  extends Model implements AuthenticatableContract, AuthorizableContract
 {
